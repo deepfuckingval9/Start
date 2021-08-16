076ed4ae53e6b3e32f67eec599c2d83552998f81
deepfuckingval9 <79183733+deepfuckingval9@users.noreply.github.com>
2021-08-16 11:00:22 +0800

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,10 @@
+#two sum
+class Solution(object):
+    def twoSum(self,nums,target):
+        if len(nums) == 0
+            return[]
+        for index,item in enumerate(nums):
+            for count in range(index+1,len(nums)):
+                for item +nums[count] ==target
+                    return[index,count]
+                    
